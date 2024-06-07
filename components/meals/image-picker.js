2d2329ce8e59600b97074ea4ba916159041508b4
Jawad Ali <jawadali66667@gmail.com>
2024-06-07 22:22:07 +0500

    Add server actions and save meal logic

---
+++ components/meals/image-picker.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInputRef = useRef();
+
+  function handleClick() {
+    imageInputRef.current.click();
+  }
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage ? (
+            "No image selected"
+          ) : (
+            <Image src={pickedImage} fill alt="Picked" />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+          required
+        />
+        <button className={classes.button} type="button" onClick={handleClick}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}

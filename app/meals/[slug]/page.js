b591c47646329081ec1d0bc516dc5532de16d365
Jawad Ali <jawadali66667@gmail.com>
2024-06-09 11:40:58 +0500

    Finish app

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,16 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) notFound();
+
+  return {
+    title: meal.title,
+    descriptions: meal.description,
+  };
+}
 export default function MealDetails({ params }) {
   const meal = getMeal(params.slug);
 
